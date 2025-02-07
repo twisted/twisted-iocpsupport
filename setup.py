f4f2501acc38690d8ba6db0851a6f7c5986251c6
0xDEADFED5 <admin@terminoid.com>
2025-02-07 01:11:47 -0800

    trim the fat

--- setup.py
+++ setup.py
@@ -7,35 +7,21 @@ import setuptools
 import sysconfig
 from Cython.Build import cythonize
 
+macros = None
 if sysconfig.get_config_var("abi_thread") == "t":
-    setuptools.setup(
-        ext_modules=cythonize(
-            [
-                setuptools.Extension(
-                    "twisted_iocpsupport.iocpsupport",
-                    sources=[
-                        "twisted_iocpsupport/iocpsupport.pyx",
-                        "twisted_iocpsupport/winsock_pointers.c",
-                    ],
-                    libraries=["ws2_32"],
-                    # GRAALVM_PYTHON happens to mostly set the defines we need
-                    define_macros=[("GRAALVM_PYTHON", "1"), ("Py_GIL_DISABLED", "1")],
-                )
-            ]
-        )
+    macros = [("GRAALVM_PYTHON", "1"), ("Py_GIL_DISABLED", "1")]
+setuptools.setup(
+    ext_modules=cythonize(
+        [
+            setuptools.Extension(
+                "twisted_iocpsupport.iocpsupport",
+                sources=[
+                    "twisted_iocpsupport/iocpsupport.pyx",
+                    "twisted_iocpsupport/winsock_pointers.c",
+                ],
+                libraries=["ws2_32"],
+                define_macros=macros,
+            )
+        ]
     )
-else:
-    setuptools.setup(
-        ext_modules=cythonize(
-            [
-                setuptools.Extension(
-                    "twisted_iocpsupport.iocpsupport",
-                    sources=[
-                        "twisted_iocpsupport/iocpsupport.pyx",
-                        "twisted_iocpsupport/winsock_pointers.c",
-                    ],
-                    libraries=["ws2_32"],
-                )
-            ]
-        )
-    )
+)
