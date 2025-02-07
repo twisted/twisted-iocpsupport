8cd2ce58a7f4caaf76479963c17934d2bbc2cc28
0xDEADFED5 <admin@terminoid.com>
2025-02-07 00:49:32 -0800

    add support for 3.13t

--- setup.py
+++ setup.py
@@ -4,20 +4,39 @@
 # See LICENSE for details.
 
 import setuptools
+import sysconfig
 from Cython.Build import cythonize
 
-
-setuptools.setup(
-    ext_modules=cythonize(
-        [
-            setuptools.Extension(
-                "twisted_iocpsupport.iocpsupport",
-                sources=[
-                    "twisted_iocpsupport/iocpsupport.pyx",
-                    "twisted_iocpsupport/winsock_pointers.c",
-                ],
-                libraries=["ws2_32"],
-            )
-        ]
+if sysconfig.get_config_var('abi_thread') == 't':
+    setuptools.setup(
+        ext_modules=cythonize(
+            [
+                
+                setuptools.Extension(
+                    "twisted_iocpsupport.iocpsupport",
+                    sources=[
+                        "twisted_iocpsupport/iocpsupport.pyx",
+                        "twisted_iocpsupport/winsock_pointers.c",
+                    ],
+                    libraries=["ws2_32"],
+                    # GRAALVM_PYTHON happens to mostly set the defines we need
+                    define_macros=[("GRAALVM_PYTHON", "1"), ("Py_GIL_DISABLED", "1")],
+                )
+            ]
+        )
     )
-)
+else:
+    setuptools.setup(
+        ext_modules=cythonize(
+            [
+                setuptools.Extension(
+                    "twisted_iocpsupport.iocpsupport",
+                    sources=[
+                        "twisted_iocpsupport/iocpsupport.pyx",
+                        "twisted_iocpsupport/winsock_pointers.c",
+                    ],
+                    libraries=["ws2_32"],
+                )
+            ]
+        )
+    )
