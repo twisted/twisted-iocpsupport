c13b9e3c5457f3c4b853e691cc132fe5797dc707
0xDEADFED5 <admin@terminoid.com>
2025-02-16 00:38:56 -0800

    formatting

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ macros = None
 # CYTHON_USE_TYPE_SPECS needs to be 0, otherwise the module won't compile
 # CYTHON_METH_FASTCALL needs to be 0, but Py_GIL_DISABLED sets it to 1
 # CYTHON_ASSUME_SAFE_MACROS needs to be 0, but Py_GIL_DISABLED sets it to 1
-# we define GRAALVM_PYTHON because it sets CYTHON_METH_FASTCALL to 0 and CYTHON_ASSUME_SAFE_MACROS to 0
+# we define GRAALVM_PYTHON because it sets CYTHON_METH_FASTCALL to 0 and 
+# CYTHON_ASSUME_SAFE_MACROS to 0
 # this should probably be re-checked every major Python version
 if sysconfig.get_config_var("Py_GIL_DISABLED") == 1:
     macros = [("GRAALVM_PYTHON", "1"), ("Py_GIL_DISABLED", "1")]
